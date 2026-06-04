04ebf9fd6d393d7fa60eee0cd60694f3d64b5b75
HugoLopesGM <hugolopesgarciamendes05@gmail.com>
2026-06-04 13:05:40 -0300

    Adicionando repository

---
+++ AEP/src/main/java/com/AEP/AEP/repository/SolicitacaoRepository.java
@@ -0,0 +1,8 @@
+package com.AEP.AEP.repository;
+
+import com.AEP.AEP.model.SolicitacaoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SolicitacaoRepository extends JpaRepository<SolicitacaoModel, Long> {
+
+}
